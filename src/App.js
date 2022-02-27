33db70fe660894cf240203e9d04912dad53e22df
bankymono <bankymono@gmail.com>
2022-02-27 17:43:45 +0100

    simple landing page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing_container">
+
+      <div className='menu_wrapper'>
+        <img className='expertnaire_logo' src='/expertnaire_logo_2.jpg' alt='brand logo' />
+      </div>
+
+      <div className='center-content-container'>
+        <div className="left-content">
+          <h2>New! Discover how High Score IELTS Program gives you the exact steps to Score A Minimum 7.5 Out Of 9... within 30 Days. ... Now!</h2>
+
+          <a target="blank" className='cta-btn' href='https://app.expertnaire.com/product/7259100822/6160378956?p=2'>BUY NOW!</a>
+        </div>
+
+        <img className="right-image" src="elizeu-dias-2.jpg" alt='student' />
+      </div>
+
+
+
+    <div className='photo-credit'>
+      Photo by <a href="https://unsplash.com/@elishavision?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Elizeu Dias</a> on <a href="https://unsplash.com/s/photos/black-man?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+      </div>
     </div>
   );
 }
